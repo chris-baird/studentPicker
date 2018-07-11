ef20d71e8a5b7f28c19a9b31daee05a0a5bfd1fa
Sammy Roberts <omg.yeah.wai@hotmail.com>
2018-07-10 19:35:09 -0700

    Update index.js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,32 +1,6 @@
 const students = [
-    "https://github.com/bseitz754",
-    "https://github.com/bpham2002",
-    "https://github.com/cameron4k",
-    "https://github.com/chieuduy",
-    "https://github.com/chriserb614",
-    "https://github.com/clopes1",
-    "https://github.com/chrisyforshort",
-    "https://github.com/gavinquirk",
-    "https://github.com/dynamicmosiac",
-    "https://github.com/floresjacob",
-    "https://github.com/jacobv2410",
-    "https://github.com/javierleon94",
-    "https://github.com/jdknappe",
-    "https://github.com/jodecile84",
-    "https://github.com/JodeciLe84",
-    "https://github.com/Dev533",
-    "https://github.com/jgsilverman",
-    "https://github.com/jphamxuan",
-    "https://github.com/khurlock",
-    "https://github.com/MariGMartinez",
-    "https://github.com/markchiang1",
-    "https://github.com/hoang-minh",
-    "https://github.com/oale444",
-    "https://github.com/PRESTONPOWELL22",
-    "https://github.com/pujasodha",
-    "https://github.com/thomasseaman",
-    "https://github.com/vfgonzalez",
-    "https://github.com/yunwhyjay"
+    "github.com/aaronsy1"
+    "github.com/alexmrtnz"
    ]
 
 function findStudent() {
